f8bb9d99f96c7f52697488b87c1cf194c28a4068
Andreas Willig <andreas.willig@canterbury.ac.nz>
2025-02-18 11:05:37 +1300

    Modified default shared memory lock timeout

--- dcp-vardis-project/dcpimplementation/src/dcp/common/global_types_constants.h
+++ dcp-vardis-project/dcpimplementation/src/dcp/common/global_types_constants.h
@@ -67,7 +67,7 @@ namespace dcp {
   /**
    * @brief Default timeout for a shared memory lock in ms
    */
-  const uint16_t   defaultSharedMemoryLockTimeoutMS = 50;
+  const uint16_t   defaultSharedMemoryLockTimeoutMS = 100;
 
 
   /**
