37fd2b957f2d993001298fcf52eac09d396f9247
Andreas Willig <andreas.willig@canterbury.ac.nz>
2025-02-27 15:44:37 +1300

    Changed parameter handling to now have sane defaults for command socket name and Vardis variable store area name (which is also new)

--- dcp-vardis-project/dcpimplementation/src/dcp/applications/vardisapp-describe-variable.cc
+++ dcp-vardis-project/dcpimplementation/src/dcp/applications/vardisapp-describe-variable.cc
@@ -4,47 +4,83 @@
 #include <dcp/vardis/vardisclient_configuration.h>
 #include <dcp/vardis/vardisclient_lib.h>
 
+using std::cerr;
 using std::cout;
 using std::endl;
+
 using namespace dcp;
 using namespace dcp::vardis;
 
-void print_help_and_exit (std::string execname)
+
+void print_version ()
 {
-  cout << execname << " <sockname> <varId>" << endl
-       << endl
-       << "Describes a Vardis variable." << endl
-       << endl
-       << "Parameters:" << endl
-       << "    sockname:     filename of Vardis command socket (UNIX Domain socket)" << endl
-       << "    varId:        variable identifier, unique value between 0 and " << (int) dcp::vardis::VarIdT::max_val() << endl;
-  exit (EXIT_SUCCESS);
+  cout << dcp::dcpHighlevelDescription
+       << " -- version " << dcp::dcpVersionNumber
+       << endl;
 }
+
 
 int main (int argc, char* argv [])
 {
-  // ----------------------------------
-  // Check parameters
+  std::string cmdsock_name  = defaultVardisCommandSocketFileName;
+  std::string shmname_cli   = "irrelevant";
+  std::string shmname_glob  = defaultVardisVariableStoreShmName;
+  int     varIdTmp = 0;
   
-  if (argc != 3)
-    print_help_and_exit (std::string (argv[0]));
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("help,h",         "produce help message and exit")
+    ("version,v",      "show version information and exit")
+    ("sockname,s",     po::value<std::string>(&cmdsock_name)->default_value(defaultVardisCommandSocketFileName), "filename of VarDis command socket (UNIX Domain Socket)")
+    ("shmgdb,g",       po::value<std::string>(&shmname_glob)->default_value(defaultVardisVariableStoreShmName), "Unique name of shared memory area for accessing VarDis variables (global database)")
+    ("varid",          po::value<int>(&varIdTmp), "Variable identifier")
+    ;
 
-  std::string sockname (argv[1]);
-  int         varIdTmp   (std::stoi(std::string(argv[2])));
+  po::positional_options_description desc_pos;
+  desc_pos.add ("varid", 1);
 
-  if ((varIdTmp < 0) || (varIdTmp > dcp::vardis::VarIdT::max_val()))
-    {
-      cout << "Parameter varId outside allowed range. Aborting." << endl;
-      return EXIT_FAILURE;
-    }
+  try {
+    po::variables_map vm;
+    //po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::store (po::command_line_parser(argc, argv).options(desc).positional(desc_pos).run(), vm);
+    po::notify(vm);
 
+    if (vm.count("help"))
+      {
+	cout << std::string (argv[0]) << " [-s <sockname>]" << endl;
+	cout << desc << endl;
+	return EXIT_SUCCESS;
+      }
+
+    if (vm.count("version"))
+      {
+	print_version();
+	return EXIT_SUCCESS;
+      }
+
+    if ((varIdTmp < 0) || (varIdTmp > dcp::vardis::VarIdT::max_val()))
+      {
+	cout << "Varid outside allowed range. Aborting." << endl;
+	return EXIT_FAILURE;
+      }    
+    
+  }
+  catch(std::exception& e) {
+    cerr << argv[0] << ": option error. Exiting." << endl;
+    cerr << e.what() << endl;
+    cerr << desc << endl;
+    return EXIT_FAILURE;
+  }
+
+  // ----------------------------------
   VarIdT    varId (varIdTmp);
   
   // ----------------------------------
   // Register with Vardis and delete variable
   VardisClientConfiguration cl_conf;
-  cl_conf.cmdsock_conf.commandSocketFile = sockname;
-  cl_conf.shm_conf.shmAreaName           = "irrelevant";
+  cl_conf.cmdsock_conf.commandSocketFile = cmdsock_name;
+  cl_conf.shm_conf_client.shmAreaName    = shmname_cli;
+  cl_conf.shm_conf_global.shmAreaName    = shmname_glob;
 
   try {
     VardisClientRuntime cl_rt (cl_conf, false);
@@ -59,21 +95,21 @@ int main (int argc, char* argv [])
       }
 
     cout << "Describing variable:\n"
-    << "   varId        = " << (int) var_descr.varId.val  << "\n"
-    << "   prodId       = " << var_descr.prodId << "\n"
-    << "   repCnt       = " << (int) var_descr.repCnt.val << "\n"
-    << "   description  = " << var_descr.description << "\n"
-    << "   seqno        = " << (int) var_descr.seqno.val << "\n"
-    << "   tStamp       = " << var_descr.tStamp << "\n"
-    << "   countUpdate  = " << (int) var_descr.countUpdate.val << "\n"
-    << "   countCreate  = " << (int) var_descr.countCreate.val << "\n"
-    << "   countDelete  = " << (int) var_descr.countDelete.val << "\n"
-    << std::boolalpha
-    << "   toBeDeleted  = " << var_descr.toBeDeleted << "\n"
-    << "   value_length = " << (int) var_descr.value_length.val << "\n"
-    << "   data         = " << byte_array_to_string (buffer, std::min (32, (int) var_descr.value_length.val)) << "\n"
-    << endl;
-							     
+	 << "   varId        = " << (int) var_descr.varId.val  << "\n"
+	 << "   prodId       = " << var_descr.prodId << "\n"
+	 << "   repCnt       = " << (int) var_descr.repCnt.val << "\n"
+	 << "   description  = " << var_descr.description << "\n"
+	 << "   seqno        = " << (int) var_descr.seqno.val << "\n"
+	 << "   tStamp       = " << var_descr.tStamp << "\n"
+	 << "   countUpdate  = " << (int) var_descr.countUpdate.val << "\n"
+	 << "   countCreate  = " << (int) var_descr.countCreate.val << "\n"
+	 << "   countDelete  = " << (int) var_descr.countDelete.val << "\n"
+	 << std::boolalpha
+	 << "   toBeDeleted  = " << var_descr.toBeDeleted << "\n"
+	 << "   value_length = " << (int) var_descr.value_length.val << "\n"
+	 << "   data         = " << byte_array_to_string (buffer, std::min (32, (int) var_descr.value_length.val)) << "\n"
+	 << endl;
+    
   }
   catch (std::exception& e)
     {
