8f85812da664f69b8012082025151be5c0340a90
Andreas Willig <andreas.willig@canterbury.ac.nz>
2025-02-27 15:11:46 +1300

    Changed parameter handling (use of default parameters), added parameter for Vardis variable store

--- dcp-vardis-project/dcpimplementation/src/dcp/applications/vardisapp-list-variables.cc
+++ dcp-vardis-project/dcpimplementation/src/dcp/applications/vardisapp-list-variables.cc
@@ -5,42 +5,80 @@
 #include <limits>
 #include <list>
 #include <thread>
+#include <boost/program_options.hpp>
 #include <dcp/common/global_types_constants.h>
+#include <dcp/vardis/vardis_constants.h>
 #include <dcp/vardis/vardis_service_primitives.h>
 #include <dcp/vardis/vardis_transmissible_types.h>
 #include <dcp/vardis/vardisclient_configuration.h>
 #include <dcp/vardis/vardisclient_lib.h>
 
+using dcp::vardis::defaultVardisCommandSocketFileName;
+using std::cerr;
 using std::cout;
 using std::endl;
+using dcp::vardis::defaultVardisVariableStoreShmName;
+
+
 using namespace dcp;
 
-void print_help_and_exit (std::string execname)
+const std::string defaultVardisClientShmName = "shm-vardisapp-list-variables";
+
+
+void print_version ()
 {
-  cout << execname << " <sockname>" << endl
-       << endl
-       << "Retrieves list of currently known variables from Vardis instance and prints it." << endl
-       << endl
-       << "Parameters:" << endl
-       << "    sockname:       filename of Vardis command socket (UNIX Domain socket)" << endl;
-  exit (EXIT_SUCCESS);
+  cout << dcp::dcpHighlevelDescription
+       << " -- version " << dcp::dcpVersionNumber
+       << endl;
 }
 
 
 int main (int argc, char* argv [])
 {
-  // ----------------------------------
-  // Check parameters
-  
-  if (argc != 2)
-    print_help_and_exit (std::string (argv[0]));
+  std::string cmdsock_name  = defaultVardisCommandSocketFileName;
+  std::string shmname_glob  = defaultVardisVariableStoreShmName;
 
-  std::string sockname (argv[1]);
+  
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("help,h",         "produce help message and exit")
+    ("version,v",      "show version information and exit")
+    ("sockname,s",     po::value<std::string>(&cmdsock_name)->default_value(defaultVardisCommandSocketFileName), "filename of VarDis command socket (UNIX Domain Socket)")
+    ("shmgdb,g",       po::value<std::string>(&shmname_glob)->default_value(defaultVardisVariableStoreShmName), "Unique name of shared memory area for accessing VarDis variables (global database)")
+    ;
+
+  try {
+    po::variables_map vm;
+    //po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::store (po::command_line_parser(argc, argv).options(desc).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help"))
+      {
+	cout << std::string (argv[0]) << " [-s <sockname>]" << endl;
+	cout << desc << endl;
+	return EXIT_SUCCESS;
+      }
+
+    if (vm.count("version"))
+      {
+	print_version();
+	return EXIT_SUCCESS;
+      }    
+  }
+  catch(std::exception& e) {
+    cerr << argv[0] << ": option error. Exiting." << endl;
+    cerr << e.what() << endl;
+    cerr << desc << endl;
+    return EXIT_FAILURE;
+  }
 
   // ----------------------------------
   // Register with Vardis
   VardisClientConfiguration cl_conf;
-  cl_conf.cmdsock_conf.commandSocketFile = sockname;
+  cl_conf.cmdsock_conf.commandSocketFile = cmdsock_name;
+  cl_conf.shm_conf_client.shmAreaName    = "irrelevant";
+  cl_conf.shm_conf_global.shmAreaName    = shmname_glob;
 
   try {
     VardisClientRuntime cl_rt (cl_conf, false);
