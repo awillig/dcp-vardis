81cf5ff42da39a0b0d2f5f9ad93bbc8dadbc66d4
Andreas Willig <andreas.willig@canterbury.ac.nz>
2025-02-27 15:19:57 +1300

    Restructured processing of command line parameters (socket name and Vardis variable store name are now optional and have default values)

--- dcp-vardis-project/dcpimplementation/src/dcp/applications/vardisapp-delete-variable.cc
+++ dcp-vardis-project/dcpimplementation/src/dcp/applications/vardisapp-delete-variable.cc
@@ -3,48 +3,85 @@
 #include <dcp/vardis/vardisclient_configuration.h>
 #include <dcp/vardis/vardisclient_lib.h>
 
+using std::cerr;
 using std::cout;
 using std::endl;
+
+using dcp::vardis::defaultVardisCommandSocketFileName;
+using dcp::vardis::defaultVardisVariableStoreShmName;
+
 using namespace dcp;
 
-void print_help_and_exit (std::string execname)
+const std::string defaultVardisClientShmName = "shm-vardisapp-delete-variable";
+
+void print_version ()
 {
-  cout << execname << " <sockname> <shmname> <varId>" << endl
-       << endl
-       << "Deletes a Vardis variable." << endl
-       << endl
-       << "Parameters:" << endl
-       << "    sockname:     filename of Vardis command socket (UNIX Domain socket)" << endl
-       << "    shmname:      unique name of shared memory area for interfacing with VarDis" << endl
-       << "    varId:        variable identifier, unique value between 0 and " << (int) dcp::vardis::VarIdT::max_val() << endl;
-  exit (EXIT_SUCCESS);
+  cout << dcp::dcpHighlevelDescription
+       << " -- version " << dcp::dcpVersionNumber
+       << endl;
 }
 
 int main (int argc, char* argv [])
 {
-  // ----------------------------------
-  // Check parameters
+  std::string cmdsock_name  = defaultVardisCommandSocketFileName;
+  std::string shmname_cli   = "irrelevant";
+  std::string shmname_glob  = defaultVardisVariableStoreShmName;
+  int     varIdTmp = 0;
   
-  if (argc != 4)
-    print_help_and_exit (std::string (argv[0]));
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("help,h",         "produce help message and exit")
+    ("version,v",      "show version information and exit")
+    ("sockname,s",     po::value<std::string>(&cmdsock_name)->default_value(defaultVardisCommandSocketFileName), "filename of VarDis command socket (UNIX Domain Socket)")
+    ("shmgdb,g",       po::value<std::string>(&shmname_glob)->default_value(defaultVardisVariableStoreShmName), "Unique name of shared memory area for accessing VarDis variables (global database)")
+    ("varid",          po::value<int>(&varIdTmp), "Variable identifier")
+    ;
 
-  std::string sockname (argv[1]);
-  std::string shmname  (argv[2]);
-  int         varIdTmp   (std::stoi(std::string(argv[3])));
+  po::positional_options_description desc_pos;
+  desc_pos.add ("varid", 1);
 
-  if ((varIdTmp < 0) || (varIdTmp > dcp::vardis::VarIdT::max_val()))
-    {
-      cout << "Parameter varId outside allowed range. Aborting." << endl;
-      return EXIT_FAILURE;
-    }
+  try {
+    po::variables_map vm;
+    po::store (po::command_line_parser(argc, argv).options(desc).positional(desc_pos).run(), vm);
+    po::notify(vm);
 
+    if (vm.count("help"))
+      {
+	cout << std::string (argv[0]) << " [-s <sockname>] [-mg <shmgdb>] <varId>" << endl;
+	cout << desc << endl;
+	return EXIT_SUCCESS;
+      }
+
+    if (vm.count("version"))
+      {
+	print_version();
+	return EXIT_SUCCESS;
+      }
+    
+    if ((varIdTmp < 0) || (varIdTmp > dcp::vardis::VarIdT::max_val()))
+      {
+	cout << "Varid outside allowed range. Aborting." << endl;
+	return EXIT_FAILURE;
+      }    
+  }
+  catch(std::exception& e) {
+    cerr << e.what() << endl;
+    cerr << desc << endl;
+    return EXIT_FAILURE;
+  }
+
+
+  
+  // ----------------------------------
   VarIdT    varId (varIdTmp);
   
   // ----------------------------------
   // Register with Vardis and delete variable
   VardisClientConfiguration cl_conf;
-  cl_conf.cmdsock_conf.commandSocketFile = sockname;
-  cl_conf.shm_conf.shmAreaName           = shmname;
+  cl_conf.cmdsock_conf.commandSocketFile = cmdsock_name;
+  cl_conf.shm_conf_client.shmAreaName    = shmname_cli;
+  cl_conf.shm_conf_global.shmAreaName    = shmname_glob;
+
 
   try {
     VardisClientRuntime cl_rt (cl_conf);
