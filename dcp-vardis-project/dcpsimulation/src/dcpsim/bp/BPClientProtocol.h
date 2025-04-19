9762f1c7030e649001a0dd65179fdf46e68de32d
Andreas Willig <andreas.willig@canterbury.ac.nz>
2025-04-19 14:34:35 +0200

    Replace include of BPDataTypes.h by equivalent import of bp_transmissible_types.h from the DCP implementation

--- dcp-vardis-project/dcpsimulation/src/dcpsim/bp/BPClientProtocol.h
+++ dcp-vardis-project/dcpsimulation/src/dcpsim/bp/BPClientProtocol.h
@@ -21,15 +21,14 @@
 #include <inet/common/packet/Message.h>
 #include <inet/common/packet/Packet.h>
 #include <dcp/bp/bp_queueing_mode.h>
+#include <dcp/bp/bp_transmissible_types.h>
 #include <dcp/common/services_status.h>
 #include <dcpsim/common/DcpProtocol.h>
-#include <dcpsim/common/DcpTypesGlobals.h>
-#include <dcpsim/bp/BPDataTypes.h>
 #include <dcpsim/bp/BPRegisterProtocol_m.h>
 #include <dcpsim/bp/BPDeregisterProtocol_m.h>
 #include <dcpsim/bp/BPConfirmation_m.h>
 
-
+using dcp::bp::BPLengthT;
 using dcp::bp::BPQueueingMode;
 
 // --------------------------------------------------------------------
