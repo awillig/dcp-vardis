e1a584a8097d13958bb13fc391f364f4bee351a8
Andreas Willig <andreas.willig@canterbury.ac.nz>
2025-04-25 11:48:21 +0200

    Fixed library to use in debug mode

--- dcp-vardis-project/dcpsimulation/setalias.sh
+++ dcp-vardis-project/dcpsimulation/setalias.sh
@@ -1,2 +1,2 @@
-alias do-oppmkmk='opp_makemake -f -r -M debug --deep -I$INET_ROOT/src -I./src -I../dcpimplementation/src -L$INET_ROOT/src -L../dcpimplementation/_build -lINET -ldcpcommon -ldcpbp -ldcpvardis'
+alias do-oppmkmk='opp_makemake -f -r -M debug --deep -I$INET_ROOT/src -I./src -I../dcpimplementation/src -L$INET_ROOT/src -L../dcpimplementation/_build -lINET_dbg -ldcpcommon -ldcpbp -ldcpvardis'
 alias do-build='make'
