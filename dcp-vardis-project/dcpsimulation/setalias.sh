5955438a930c35d3a6491e9b9fd6bad3e5669a13
Andreas Willig <andreas.willig@canterbury.ac.nz>
2025-04-26 09:30:21 +0200

    Separated out aliases for opp_makemake, one for debug and one for release modes

--- dcp-vardis-project/dcpsimulation/setalias.sh
+++ dcp-vardis-project/dcpsimulation/setalias.sh
@@ -1,2 +1,3 @@
-alias do-oppmkmk='opp_makemake -f -r -M debug --deep -I$INET_ROOT/src -I./src -I../dcpimplementation/src -L$INET_ROOT/src -L../dcpimplementation/_build -lINET_dbg -ldcplib-common-sim -ldcplib-bp-sim -ldcplib-vardis-sim'
+alias do-oppmkmk-dbg='opp_makemake -f -r -M debug --deep -I$INET_ROOT/src -I./src -I../dcpimplementation/src -L$INET_ROOT/src -L../dcpimplementation/_build -lINET_dbg -ldcplib-common-sim -ldcplib-bp-sim -ldcplib-vardis-sim'
+alias do-oppmkmk-rel='opp_makemake -f -r -M release --deep -I$INET_ROOT/src -I./src -I../dcpimplementation/src -L$INET_ROOT/src -L../dcpimplementation/_build -lINET -ldcplib-common-sim -ldcplib-bp-sim -ldcplib-vardis-sim'
 alias do-build='make'
